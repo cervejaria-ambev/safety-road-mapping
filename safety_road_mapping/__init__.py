8dba4344714ef1af50679302ed1e67e946540ad7
Gabriel Aparecido Fonseca <99823045@ambev.com.br>
2021-10-04 09:52:20 -0300

    Change __init__.py file to reflect the current version

--- safety_road_mapping/__init__.py
+++ safety_road_mapping/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '0.1.0'
+def get_version():
+    import re
+    toml_file = open('../pyproject.toml').read()
+    return re.findall(r'(?<=version = ").*(?="\n)', toml_file)[0]
+
+
+__version__ = get_version()
